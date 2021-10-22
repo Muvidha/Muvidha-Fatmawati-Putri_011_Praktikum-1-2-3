89041b50b81c2e0fa9b5b8d676dc0b82328e921b
Muvidha <90436781+Muvidha@users.noreply.github.com>
2021-10-22 16:26:17 +0700

    Add files via upload

---
+++ Muvidha Fatmawati Putri_011_SEGITIGA.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+#include <conio.h>
+#include <math.h>
+using namespace std;
+
+int main()
+{
+	float tinggi, alas;
+	cout<< " Menghitung Segitiga"<<endl;
+	cout<<"\n";
+	
+	cout << " Masukkan Tinggi : ";
+	cin >> tinggi;
+	cout << " Masukkan Alas : ";
+	cin >> alas;
+	
+	cout << " Sisi Miring = "<<sqrt(alas*alas+tinggi*tinggi)<<endl;
+	cout << " Luas = "<<0.5*alas*tinggi<<endl;
+	cout << " Keliling = "<<tinggi+alas+sqrt(alas*alas+tinggi*tinggi)<<endl;
+	
+	return 0;
+}
